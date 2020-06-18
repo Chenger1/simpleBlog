86b38e494a9016bf2db23d8d10b3325175ea0cce
Vladislav <exs2199@gmail.com>
2020-06-18 19:21:16 +0300

    Add client structure using Vue.js

---
+++ client/blog/src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="navbar navbar-dark bg-dark">
+        <h1 class="navbar-brand">
+            {{sitename}}
+        </h1>
+    </div> 
+</template>
+<script>
+export default {
+    name: 'my-header',
+    data (){
+        sitename: 'simpleBlog'
+    }
+}
+</script>
